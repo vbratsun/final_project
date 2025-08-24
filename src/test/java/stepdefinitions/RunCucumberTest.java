bed359a61aea0d37c546f621e902113873ac9a8e
Vladimir Bratsiun <v.braciun@insitech.dev>
2025-08-24 23:06:00 +0300

    - добывил потерянный файл

---
+++ src/test/java/stepdefinitions/RunCucumberTest.java
@@ -0,0 +1,9 @@
+package stepdefinitions;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("stepdefinitions")
+public class RunCucumberTest {
+}
